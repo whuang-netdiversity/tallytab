960094c7030e8ff20d1632123cea5d06fa12f4a5
Wes Huang <ops@netdiversity.ca>
2025-08-16 17:47:37 -0600

    Added receipt key

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,4 +1,5 @@
 // Storage keys
+export const DRAFT_KEY = 'is_draft';
 export const PATRONS_KEY = 'tally_tab:patrons';
 export const BILL_KEY = 'tally_tab:bill';
 export const TALLY_KEY = BILL_KEY; // single canonical store
@@ -6,3 +7,4 @@ export const TALLY_KEY = BILL_KEY; // single canonical store
 // UI/event keys (page/interface ids)
 export const BILL_EVENT_KEY = 'bill-3987';
 export const TALLY_EVENT_KEY = 'tally-3987';
+export const RECEIPT_EVENT_KEY = 'receipt-9312';
