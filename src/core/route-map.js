10fe485fc52ae3e2bbedb8175afb5194bf842c09
Wes Huang <ops@netdiversity.ca>
2025-08-19 16:08:39 -0600

    Added new route

--- src/core/route-map.js
+++ src/core/route-map.js
@@ -1,7 +1,10 @@
 import { patron_detail } from '@/pages/patron-detail';
 import { patronDetailsRoute } from '@/pages/patron-detail';
+import { bills_detail } from '@/pages/bills_detail';
+import { billsDetailsRoute } from '@/pages/bills_detail';
 
 export const routeMap = {
     [patron_detail.key]: (params) => patronDetailsRoute(params),
+    [bills_detail.key]: () => billsDetailsRoute()
     // Add more as needed
 };
