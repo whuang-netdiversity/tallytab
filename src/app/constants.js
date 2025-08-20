d3e7337e8f9b27fae9a75f5699a62c8309b34e5d
Wes Huang <ops@netdiversity.ca>
2025-08-20 10:09:48 -0600

    Added key

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,5 +1,6 @@
 // Storage keys
 export const DRAFT_KEY = 'is_draft';
+export const HISTORY_KEY = 'tally_tab:history';
 export const PATRONS_KEY = 'tally_tab:patrons';
 export const BILL_KEY = 'tally_tab:bill';
 export const TALLY_KEY = BILL_KEY; // single canonical store
